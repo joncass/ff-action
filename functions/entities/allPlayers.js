40b1794e90da94a8c8c3930660c9e15099fc42fe
Jon Cass <joncass@gmail.com>
2017-10-06 19:28:42 -0400

    install jest and move rankings api into api/

--- functions/entities/allPlayers.js
+++ functions/entities/allPlayers.js
@@ -1,7 +1,7 @@
-const rankings = require('../data/rankings')
+const rankingsAPI = require('../api/rankings')
 
 const printAllPlayers = () => {
-  rankings.getAllPositionRankings.then(positionalRankingSet => {
+  rankingsAPI.getAllPositionRankings.then(positionalRankingSet => {
     positionalRankingSet.forEach(positionalRankings => {
       positionalRankings.forEach(player => {
         console.log(`"${player.firstName} ${player.lastName}","${player.firstName} ${player.lastName}"`)
