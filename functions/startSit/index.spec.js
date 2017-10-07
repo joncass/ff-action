3b75a85b5219e00b9bd7eed7aa94527bf8c1bb4f
Jon Cass <joncass@gmail.com>
2017-10-07 17:19:02 -0400

    tests for startSit index

--- functions/startSit/index.spec.js
+++ functions/startSit/index.spec.js
@@ -1,13 +1,58 @@
 const startSit = require('./index')
 
-describe('startSit', () => {
-  it('should ...', () => {
+const nflAPI = require('../api/nfl')
+const result = require('./result')
+
+describe('startSit.decision', () => {
+  it('should call the API and construct a result', () => {
+    const mockAPI = jest.fn()
+    nflAPI.getPlayerProjections = mockAPI
+    mockAPI.mockReturnValue(
+      new Promise((resolve, reject) => {
+        process.nextTick(
+          () => resolve([10.1, 12.2]),
+        )
+      })
+    )
+
+    const mockConstructResult = jest.fn()
+    result.constructResult = mockConstructResult
+    mockConstructResult.mockReturnValue(`Decision`)
+
+    expect.assertions(5)
+
+    expect(mockAPI).not.toHaveBeenCalled()
+    expect(mockConstructResult).not.toHaveBeenCalled()
+    return startSit.decision({
+      players: [`First Player`, `Second Player`],
+    }).then(decision => {
+      expect(mockAPI).toHaveBeenCalledWith({
+        players: [`First Player`, `Second Player`],
+      })
+      expect(mockConstructResult).toHaveBeenCalledWith([
+        {
+          name: `First Player`,
+          projection: 10.1,
+        },
+        {
+          name: `Second Player`,
+          projection: 12.2,
+        },
+      ])
+      expect(decision).toBe(`Decision`)
+    })
+  })
+
+  it('should reject with an error if the API throws', () => {
+    nflAPI.getPlayerProjections = () => new Promise((resolve, reject) => {
+      throw new Error(`API Error`)
+    })
+
     expect.assertions(1)
     return startSit.decision({
-      players: ['Aaron Rodgers', `Aaron Rodgers`],
-    }).then(decision => {
-      console.log(decision)
-      expect(1).toBe(1)
+      players: [`First Player`, `Second Player`],
+    }).catch(error => {
+      expect(error).toEqual(Error(`API Error`))
     })
   })
 })
