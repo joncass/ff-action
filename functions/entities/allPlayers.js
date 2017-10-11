640f988ad09efb944deab946ff60c8c71ea8bd4f
Jon Cass <joncass@gmail.com>
2017-10-11 19:54:05 -0400

    entity names with and without punctuation

--- functions/entities/allPlayers.js
+++ functions/entities/allPlayers.js
@@ -1,5 +1,11 @@
 const nflAPI = require('../api/nfl')
 
 nflAPI.getAllPlayerNames().then(names => {
-  names.forEach(name => console.log(`"${name}","${name}"`))
+  names.forEach(name => {
+    const strippedName = name.replace(/[^\w\s]/g, '')
+    console.log(`"${name}","${name}"`)
+    if (strippedName !== name) {
+      console.log(`"${strippedName}","${strippedName}"`)
+    }
+  })
 })
